14971b75b5f5b6861538918e84703e59b7f17a55
yaflow-droid <yaflowart@gmail.com>
2020-04-09 16:03:58 +0200

    Added nsfw tag and setting

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,13 @@
+$(document).on(':passageinit', function (ev) {
+	if (settings.nsfw) {
+		console.log("NSFW ON");
+		$("html").removeClass("hidden");	
+	} else {
+		console.log("NSFW OFF");
+		$("html").addClass("hidden");	
+	}
+})
+
 $(document).on(':passagestart', function (ev) {
 	if (!ev.passage.tags.includes('noreturn')) {
 		State.variables.return = ev.passage.title;
@@ -6,4 +16,9 @@ $(document).on(':passagestart', function (ev) {
 	else if (!ev.passage.tags.includes('mainMenu')) {
 		State.variables.exitMenu = ev.passage.title;
 	}
+});
+
+Setting.addToggle("nsfw", {
+	label    : "Displays NSFW images",
+	default  : true
 });
