ba84cce26c3798db0e3efc83985976bcf5cdcadd
yaflow-droid <yaflow.art@gmail.com>
2020-05-17 02:32:29 +0200

    Added base code for saving

---
+++ src/javascript/save.js
@@ -0,0 +1,30 @@
+window.upgradeSave = function(save) {
+	switch (save.version) {
+		case 1:
+			// Upgrades to vertion 2
+			return save;
+		default:
+			return null
+	}
+}
+
+Config.saves.version = 1;
+Config.saves.slots = 8;
+Config.saves.onSave = function (save) {
+	save.title = State.passage;
+	return save;
+};
+Config.saves.onLoad = function (save) {
+	while (save.version != Config.saves.version) {
+		upgradeSave(save);
+	}
+	return save;
+};
+Config.saves.isAllowed = function () {
+	// Dissalow when passage has tag noSave
+	return !Story.get(State.passage).tags.includes("noSave");
+};
+Config.saves.autosave = function () {
+	// Make autosave when passage has tag Save
+	return Story.get(State.passage).tags.includes("Save");
+}
